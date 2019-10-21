68ddc61711a889aa9f86f6a3e7b0637fe56ed37d
Clinton Dreisbach <clinton@dreisbach.us>
2019-10-21 16:01:44 -0400

    Add django-debug-toolbar

--- config/settings.py
+++ config/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
 
     # Third-party
     'markdownify',
+    'debug_toolbar',
 
     # Mine
     'listo',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
--- config/urls.py
+++ config/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from listo import views
 
 urlpatterns = [
@@ -22,3 +23,12 @@ urlpatterns = [
     path('lists/<id>/', views.checklists_detail, name="checklists_detail"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
